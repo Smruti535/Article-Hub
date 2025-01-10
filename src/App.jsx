2b762e667f7dec76c6026aa43837ef6b98ef24a9
Smruti535 <138751739+Smruti535@users.noreply.github.com>
2025-01-10 15:24:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/Homepage';
 import ArticleDetails from './pages/ArticleDetails';
 import BookmarkPage from './pages/BookmarkPage';
 import Header from './components/Header';
